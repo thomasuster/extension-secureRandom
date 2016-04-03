6889c334732c5e6fa01972505f818a08bee63b4b
Thomas Uster <thomasuster@gmail.com>
2016-04-03 13:14:24 -0700

    simplify java impl

--- android/src/com/thomasuster/SecureRandom.java
+++ android/src/com/thomasuster/SecureRandom.java
@@ -6,17 +6,8 @@ public class SecureRandom extends Extension {
 
     public static int getSecureRandom32() {
         java.security.SecureRandom sr = new java.security.SecureRandom();
-        byte[] output = new byte[32];
+        byte[] output = new byte[4];
         sr.nextBytes(output);
-        return toInt(output);
-    }
-
-    public static int toInt(byte[] bytes) {
-        int ret = 0;
-        for (int i=0; i<4 && i<bytes.length; i++) {
-            ret <<= 8;
-            ret |= (int)bytes[i] & 0xFF;
-        }
-        return ret;
+        return output[0]
     }
 }
